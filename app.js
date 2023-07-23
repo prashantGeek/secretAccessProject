292613114d279da94f375f3e079f68c843ac1bc1
pras1907 <pt15052000@gmail.com>
2023-07-24 00:22:38 +0530

    level 5 completed

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express= require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const bcrypt= require("bcrypt")
-const saltRounds=10
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express()
 
@@ -13,16 +14,30 @@ app.use(express.static("public"))
 app.set("view engine", "ejs" )
 app.use(bodyParser.urlencoded({extended:true}))
 
+//app.use for session to be placed specifically after all the "app.use and app.set" and above mongoose.connect
+app.use(session({
+  secret:"Our little secret.",          //to sign the session ID Cookie.
+  resave:false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB")
 
-const userSchema= new mongoose.Schema({
+const userSchema= new mongoose.Schema({        //mongoose word is added to make it a mongoose schema, not a simple js Object
   email: String,
   password: String
 })
 
-
+userSchema.plugin(passportLocalMongoose);
 
 const User= mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get("/", function(req,res){
   res.render("home")
@@ -36,38 +51,52 @@ app.get("/register", function(req,res){
   res.render("register")
 })
 
+app.get("/secrets", function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets")
+  }
+  else{
+    res.redirect("login")
+  }
+})
+
+app.get("/logout", function(req,res){
+  req.logout(function(err){
+    if(err){
+      console.log(err)
+    }else{
+      res.redirect("/")
+    }
+  });
+  
+})
+
 app.post("/register", function(req,res){
-  bcrypt.hash(req.body.password, saltRounds, function(err,hash){
-    const newUser= new User({
-      email: req.body.username,
-      password: hash
-    })
-    newUser.save().then(function(done,err){
-      if(!err){
-        res.render("secrets")
-      }
-      else{
-        console.log(err)
-      }
-    })
+  User.register({username: req.body.username}, req.body.password, function(err, user){   //passport method
+    if(err){
+      console.log(err)
+      res.redirect("/register")
+    }
+    else{
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/secrets")
+      })
+    }
   })
 })
 
 app.post("/login", function(req,res){
-  const username = req.body.username
-  const password = req.body.password
-  User.findOne({email:username}).then(function(foundUser,err){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+  req.login(user, function(err){                 //passport method
     if(err){
       console.log(err)
-    }
-    else{
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result){
-          if(result===true){
-            res.render("secrets")
-          }
-        }) 
-      }
+    }else{
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/secrets")
+      })
     }
   })
 })
